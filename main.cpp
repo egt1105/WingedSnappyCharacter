487af515c19ea3df12cd7e456ca8ec432f14b461
egt <>
2021-04-16 13:03:59 +0000

    a-b

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main ()
 {
   double a,b;
+  std::cout<<"Input two numbers a and b: ";
   std::cin>>a>>b;
-  std::cout<<a+b<<endl; 
+  std::cout<<"a-b = "<<a-b<<endl; 
  }
   
